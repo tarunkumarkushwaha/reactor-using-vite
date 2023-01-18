4eafaad450ca12c028ef2faeca4f22c7315d3f72
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-01-18 19:08:17 +0530

    copy function

--- src/Components/Notepad/Notepad.jsx
+++ src/Components/Notepad/Notepad.jsx
@@ -1,29 +1,42 @@
 import React, { useState } from 'react'
 import Navbar from '../navbar'
 function Notepad() {
-const [notes, setNotes] = useState("")
-const upclick=()=>{
-  setNotes(notes.toUpperCase())
-  // console.log(notes)
-}
-const downclick=()=>{
-  setNotes(notes.toLowerCase())
-  // console.log(notes)
-}
-const capital1st =()=>{
-   let arr = notes.split(" ")
-   let arr2 = arr.map((n)=>n.slice(0,1).toUpperCase().concat(n.slice(1).toLowerCase()))
-   setNotes(arr2.join(" "))
-  //  console.log(arr2.join(" "))
-}
-const eraseall =()=>{setNotes("")}
-const displaychange=(e)=>{
-      setNotes(e.target.value)
-}
-    return (
-        <>
-            <Navbar/>
-            <div className="btn-list flex-row-center">
+  const [notes, setNotes] = useState("")
+  const [select, setSelect] = useState("")
+  const upclick = () => {
+    setNotes(notes.toUpperCase())
+  }
+  const copy = () => {
+    let clipbrd = document.getElementById("textbox")
+    clipbrd.select()
+    navigator.clipboard.writeText(clipbrd.value)
+  }
+  
+  // const cut = (e) => {
+  //   let clipbrd = document.getElementById("textbox")
+  //   // var seto = clipbrd.current.select()
+  //   console.log(e.target.focus);
+  // }
+  const downclick = () => {
+    setNotes(notes.toLowerCase())
+  }
+  const capital1st = () => {
+    let arr = notes.split(" ")
+    let arr2 = arr.map((n) => n.slice(0, 1).toUpperCase().concat(n.slice(1).toLowerCase()))
+    setNotes(arr2.join(" "))
+    //  console.log(arr2.join(" "))
+  }
+  const eraseall = () => { setNotes("") }
+  const displaychange = (e) => {
+    setNotes(e.target.value)
+  }
+  const displaychange2 = (e) => {
+    setSelect(e.target.value)
+  }
+  return (
+    <>
+      <Navbar />
+      <div className="btn-list flex-row-center">
         <button className="text-center" onClick={capital1st}>
           Capitalize 1st letter
         </button>
@@ -36,20 +49,25 @@ const displaychange=(e)=>{
         <button className="text-center" onClick={downclick}>
           All Lower case
         </button>
+        <button className="text-center" onClick={copy}>
+          Copy
+        </button>
+        
       </div>
-            <div className="flex-row-center">
-                <textarea
-                    rows="10" cols="100"
-                    value={notes}
-                    onChange={displaychange}
-                    placeholder="write something..."
-                    className="outer2 border-black"
-                ></textarea>
-            </div>
+      <div className="flex-row-center">
+        <textarea
+          id='textbox'
+          rows="10" cols="100"
+          value={notes}
+          onChange={displaychange}
+          placeholder="write something..."
+          className="outer2 border-black"
+        ></textarea>
+      </div>
+      <div className='border-black to-do'>no of words - {notes.split(" ").length} and no of characters - {notes.length}</div>
+    </>
 
-        </>
-
-    )
+  )
 }
 
 export default Notepad
