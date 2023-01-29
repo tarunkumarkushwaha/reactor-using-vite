d2d44f2f1565f996024a4e7ec58b5e35cf17953b
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-01-29 16:40:36 +0530

    Todo localstorage

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,53 +1,43 @@
 import React, { useState } from "react";
 import Navbar from "./navbar";
-const Todo = () => {
-  const [item, setItem] = useState("");
-  const [todo, setTodo] = useState([]);
-  const [goals, setGoals] = useState([]);
-  const [key, setKey] = useState(false);
 
+const Todo = () => {
+  // states 
+  const [item, setItem] = useState("");
+  const [todo, setTodo] = useState(localStorage.getItem("items") ? [localStorage.getItem("items")] : []);
+
+  // display change event 
   const displaychange = (e) => {
     setItem(e.target.value);
   };
-  const clickevent = () => {
-    // very important settodo returns the initial state in the userstate
+  // button event 
+  const addtodo = () => {
+
+    // settodo returns the initial state in the userstate
     // it works as Array.push method 
     setTodo((olditems) => {
       return [...olditems, item]
     })
+
     //  it clears the input field 
     setItem('')
+
     // save to localStorage
-    localStorage.setItem("items",todo) 
-
-    // it works as Array.push method 
-    setGoals((olditems) => {
-      return [...olditems, key]})
+    localStorage.setItem("items", todo)
   };
-
-  const current =()=>{
-    alert(goals)
-  }
   const Delete = (a) => {
     setTodo([])
     localStorage.removeItem("items")
   }
   const deletetodo = (a) => {
-    setTodo(todo.filter((e)=>{return e!==a}))
-    localStorage.setItem("items",todo)
+    setTodo(todo.filter((e) => { return e !== a }))
   }
-  const checkbx = () => {
-   key?setKey(false):setKey(true)
-  
-  // setGoals((olditems)=>{return[...olditems,hoho.value]})
-  // localStorage.setItem("targets",goals)   
+  const onEnterPress = (e) => {
+    if (e.keyCode == 13 && e.shiftKey == false) {
+      e.preventDefault();
+      addtodo();
+    }
   }
-//   checkbx().then(()=>{
-//   let id = todo.indexOf(a)+"todoitem"
-//   let hoho = document.getElementById(id)
-//   setGoals(hoho.value)
-// //  setGoals(key)
-// })
 
   return (
     <>
@@ -57,28 +47,25 @@ const Todo = () => {
           rows="4" cols="30"
           value={item}
           onChange={displaychange}
+          onKeyDown={onEnterPress}
           placeholder="enter your task"
           className="outer text-center border-black input1"
         ></textarea>
         <div className="flex-column-center">
-          <button className="text-center" onClick={clickevent}>
+          <button className="text-center" onClick={addtodo}>
             Click to add a To-do
           </button>
           <button className="text-center" onClick={Delete}>
-            Erase all
-          </button>
-          <button className="text-center" onClick={current}>
-            current
+            Reset
           </button>
         </div>
         <div className="outer text-center border-black">
           {todo.map((a) => {
-            return <div key ={todo.indexOf(a)+1} className="tododiv border-black">
-              <p className="to-do"><input type="checkbox" id={todo.indexOf(a)+"todoitem"} value={key} onClick={()=>{checkbx()}} /> {" "}
-              {todo.indexOf(a)+1}. {a}
-              
-            </p>
-            <b className="deletetodo" onClick={() => {deletetodo(a)}}>X</b>
+            return <div key={todo.indexOf(a) + 1} className="tododiv border-black">
+              <p className="to-do" id={todo.indexOf(a) + "todoitem"}> {" "}
+                {todo.indexOf(a) + 1}. {a}
+              </p>
+              <b className="deletetodo" onClick={() => { deletetodo(a) }}>X</b>
             </div>
           })}
         </div>
